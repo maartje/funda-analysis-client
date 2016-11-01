27611bba0fef5503f0ddd283a8e57671bf3d9e74
Maartje de Jonge <maartjedejonge@gmail.com>
2016-11-01 13:42:15 +0000

    add line charts

--- src/linechart.js
+++ src/linechart.js
@@ -1,37 +1,83 @@
 /*global d3*/
 /*global $*/
 
-$("body").append(`    
-    <div id='linechart-tooltip' class='hidden tooltip'>
-        <p><strong><span id="linechart-x-value"></span></strong></p>
-        <p><span id="linechart-y-value"></span></p>
-    </div>`);
+d3.json("data/means_per_month_amsterdam.json", function(data) {
+    drawMultiTrendLineChart(
+        data.means,
+        null,
+        function(d) {
+            var date = new Date(d.verkoopdatum_jaar, d.verkoopdatum_maand, 1);
+            return date;
+        },
+        function(d) {
+            return Math.round(d.ppm2);
+        },
+        "Vraagprijs per Vierkante Meter",
+        "euro",
+        "#plot-ppm2-tijd",
+        510,
+        300
+    );
+    drawMultiTrendLineChart(
+        data.means,
+        null,
+        function(d) {
+            var date = new Date(d.verkoopdatum_jaar, d.verkoopdatum_maand, 1);
+            return date;
+        },
+        function(d) {
+            return Math.round(d.looptijd_in_dagen);
+        },
+        "Looptijd in Dagen",
+        "dagen",
+        "#plot-looptijd-tijd",
+        510,
+        300
+    );
+});
 
-var drawMultiTrendLineChart = function(
-    datasets,
+d3.json("data/means_per_month.json", function(data) {
+    drawMultiTrendLineChart(
+        data.means,
+        function(d) {
+            return d.gemeente;
+        },
+        function(d) {
+            var date = new Date(d.verkoopdatum_jaar, d.verkoopdatum_maand, 1);
+            return date;
+        },
+        function(d) {
+            return Math.round(d.ppm2);
+        },
+        "Vraagprijs per Vierkante Meter",
+        "euro",
+        "#plot-ppm2-tijd-steden"
+    );
+});
+
+
+function drawMultiTrendLineChart(
+    data,
+    getKey,
     getX,
     getY,
     plotTitle = "",
     yUnit = "",
     selector = "body",
-    w = 800,
-    h = 400) {
+    w = 700,
+    h = 350) {
+        
     var margin = {
             top: 50,
             bottom: 70,
-            right: 200,
+            right: getKey? 200 : 10,
             left: 50
         },
         width = w - margin.left - margin.right,
-        height = h - margin.top - margin.bottom,
-        values = datasets.reduce(
-            function(result, dataset) {
-                return result.concat(dataset['values']);
-            }, []);
-
+        height = h - margin.top - margin.bottom;
 
     var xScale = d3.time.scale()
-        .domain(d3.extent(values, getX))
+        .domain(d3.extent(data, getX))
         .range([0, width]);
 
     var xAxis = d3.svg.axis()
@@ -60,17 +106,20 @@ var drawMultiTrendLineChart = function(
     };
 
     var yScale = d3.scale.linear()
-        .domain(niceExtent(values, getY))
+        .domain(niceExtent(data, getY))
         .range([height, 0]);
 
+    var yTicks = Math.round(2*height/100);
     var yAxis = d3.svg.axis()
         .orient("left")
-        .scale(yScale);
+        .scale(yScale)
+        .ticks(yTicks);
 
     var make_y_axis = function() {
         return d3.svg.axis()
             .orient("left")
-            .scale(yScale);
+            .scale(yScale)
+            .ticks(yTicks);
     };
     
     var line = d3.svg.line()
@@ -81,17 +130,6 @@ var drawMultiTrendLineChart = function(
             return yScale(getY(d));
         });
 
-    
-    var colors = [
-        "#e41a1c",
-        "#377eb8",
-        "#4daf4a",
-        "#984ea3",
-        "#ff7f00",
-        "#ffff33",
-        "#a65628",
-        "#f781bf",
-        "#999999"];
 
     var svg = d3.select(selector).append("svg")
         .attr("class", "line-chart")
@@ -100,30 +138,44 @@ var drawMultiTrendLineChart = function(
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var keys = datasets.map(function(o){ return o.key});
-    var colorOrdinalScale = d3.scale.ordinal().domain(keys).range(colors.slice(0, keys.length));
-    var verticalLegend = d3.svg.legend()
-                               .labelFormat("none")
-                               .cellPadding(15)
-                               .orientation("vertical")
-                               .units("")
-                               .cellWidth(40)
-                               .cellHeight(5)
-                               .inputScale(colorOrdinalScale)
-                               .cellStepping(10);
-    
     svg.append("g")
-       .attr("transform", "translate(" + (width + 20) + ",0)")
-       .attr("class", "legend")
-       .call(verticalLegend);
+        .attr("class", "xaxis axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
 
-    svg.selectAll(".legendCells rect")
-       .style("stroke-width", "0px");
-       
+    svg.append("g")
+        .attr("class", "axis")
+        .call(yAxis);
 
-    for (var i = 0; i < datasets.length; i++){
-        var dataset = datasets[i].values;
-        var color = colors[i];
+    svg.append("g")
+        .attr("class", "grid")
+        .call(make_y_axis().tickSize(-width, 0, 0).tickFormat(""));
+
+    svg.append("g")
+        .attr("class", "grid")
+        .attr("transform", "translate(0," + height + ")")
+        .call(make_x_axis().tickSize(-height, 0, 0).tickFormat(""));
+
+    svg.selectAll(".xaxis text")
+        .attr("transform", function(d) {
+            return "translate(" + -1 * this.getBBox().height + "," + (0.5 * this.getBBox().width + 5) + ")rotate(-90)";
+        });
+
+    svg.append("text")
+        .attr("class", "plot-title")
+        .attr("text-anchor", "middle") 
+        .attr("transform", "translate(" + (width / 2) + "," + (-0.5 * margin.top + 6) + ")") // centre below axis
+        .text(plotTitle);
+
+    if (!$("linechart-tooltip").length){
+         $("body").append(`    
+            <div id='linechart-tooltip' class='hidden tooltip'>
+                <p><strong><span id="linechart-x-value"></span></strong></p>
+                <p><span id="linechart-y-value"></span></p>
+            </div>`);
+    }
+    
+    var drawLine = function(dataset, color){
         var g = svg.append("g");
         g.append("path")
             .data([dataset])
@@ -166,60 +218,53 @@ var drawMultiTrendLineChart = function(
             })
             .on("mouseout", function() {
                 d3.select("#linechart-tooltip").classed("hidden", true);
-            });
+            });        
+    };
+
+    var colors = [
+        "#e41a1c",
+        "#377eb8",
+        "#4daf4a",
+        "#984ea3",
+        "#ff7f00",
+        "#ffff33",
+        "#a65628",
+        "#f781bf",
+        "#999999"];
+
+    if (!getKey){ //single line chart
+        drawLine(data, colors[0]);
+    }
+    else { //multiline chart
+        var datasets = d3.nest()
+            .key(getKey)
+            .entries(data);
+    
+        var keys = datasets.map(function(o){ return o.key});
+        var colorOrdinalScale = d3.scale.ordinal().domain(keys).range(colors.slice(0, keys.length));
+        var verticalLegend = d3.svg.legend()
+                                   .labelFormat("none")
+                                   .cellPadding(15)
+                                   .orientation("vertical")
+                                   .units("")
+                                   .cellWidth(40)
+                                   .cellHeight(5)
+                                   .inputScale(colorOrdinalScale)
+                                   .cellStepping(10);
+        
+        svg.append("g")
+           .attr("transform", "translate(" + (width + 20) + ",5)")
+           .attr("class", "legend")
+           .call(verticalLegend);
+    
+        svg.selectAll(".legendCells rect")
+           .style("stroke-width", "0px");
+           
+    
+        for (var i = 0; i < datasets.length; i++){
+            var dataset = datasets[i].values;
+            drawLine(dataset, colors[i]);
+        }        
     }
 
-
-    svg.append("g")
-        .attr("class", "xaxis axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-
-    svg.append("g")
-        .attr("class", "axis")
-        .call(yAxis);
-
-    svg.append("g")
-        .attr("class", "grid")
-        .call(make_y_axis().tickSize(-width, 0, 0).tickFormat(""));
-
-    svg.append("g")
-        .attr("class", "grid")
-        .attr("transform", "translate(0," + height + ")")
-        .call(make_x_axis().tickSize(-height, 0, 0).tickFormat(""));
-
-    svg.selectAll(".xaxis text")
-        .attr("transform", function(d) {
-            return "translate(" + -1 * this.getBBox().height + "," + (0.5 * this.getBBox().width + 5) + ")rotate(-90)";
-        });
-
-    svg.append("text")
-        .attr("class", "plot-title")
-        .attr("text-anchor", "middle") 
-        .attr("transform", "translate(" + (width / 2) + "," + (-0.5 * margin.top + 6) + ")") // centre below axis
-        .text(plotTitle);
-
-
-};
-
-d3.json("data/means_per_month.json", function(data) {
-    var meansPerGemeente = d3.nest()
-        .key(function(d) {
-            return d.gemeente;
-        })
-        .entries(data.means);
-
-    drawMultiTrendLineChart(
-            meansPerGemeente,
-            function(d) {
-                var date = new Date(d.verkoopdatum_jaar, d.verkoopdatum_maand, 1);
-                return date;
-            },
-            function(d) {
-                return Math.round(d.ppm2);
-            },
-            "Vraagprijs per Vierkante Meter",
-            "euro",
-            "#plot-ppm2-tijd"
-        );
-});
+}
